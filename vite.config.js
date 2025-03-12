2e88052881f9936d3ae54003d53ee2c2a2c61f15
ViolentUJJWAL <ujjwal21aman08@gmail.com>
2025-03-12 11:06:09 +0530

    link change

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ export default defineConfig({
     port: 10000,  // Ensure this matches the port used in Render
     host: "0.0.0.0",
     strictPort: true,  // Ensures Vite only runs on the specified port
-    allowedHosts: ["https://demo-barcode.onrender.com"], // Add your Render domain here
+    allowedHosts: ["demo-barcode.onrender.com"], // Add your Render domain here
   },
   preview: {
     port: 10000,
     host: "0.0.0.0",
     strictPort: true,
-    allowedHosts: ["https://demo-barcode.onrender.com"],
+    allowedHosts: ["demo-barcode.onrender.com"],
   },
 });
